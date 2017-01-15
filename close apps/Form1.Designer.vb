8bd90e07a3a325fa84971f755b42e630e417f805
JackGrence <green1181772@gmail.com>
2017-01-15 20:14:06 +0800

    backup

---
+++ close apps/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(28, 23)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.TextBox1.Size = New System.Drawing.Size(311, 128)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TabStop = False
+        Me.TextBox1.Text = "GTA5"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(28, 160)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(73, 31)
+        Me.Button1.TabIndex = 1
+        Me.Button1.TabStop = False
+        Me.Button1.Text = "加入程序"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(266, 160)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(73, 31)
+        Me.Button2.TabIndex = 2
+        Me.Button2.TabStop = False
+        Me.Button2.Text = "全部清除"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(147, 160)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(73, 31)
+        Me.Button3.TabIndex = 3
+        Me.Button3.TabStop = False
+        Me.Button3.Text = "上一步"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.BackColor = System.Drawing.SystemColors.Control
+        Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox2.Font = New System.Drawing.Font("微軟正黑體", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TextBox2.Location = New System.Drawing.Point(28, 219)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(311, 29)
+        Me.TextBox2.TabIndex = 5
+        Me.TextBox2.TabStop = False
+        Me.TextBox2.Text = "按一下變更(目前：F12)"
+        Me.TextBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(377, 271)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("微軟正黑體", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow
+        Me.ImeMode = System.Windows.Forms.ImeMode.Close
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "關閉程式"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+
+End Class
---
+++ close apps/Form1.vb
@@ -0,0 +1,111 @@
+﻿
+
+
+Public Class Form1
+    Dim hotkey As Integer = 123
+    Dim msdown As Boolean = False
+
+    Private Declare Function GetAsyncKeyState Lib "user32.dll" (ByVal vkey As Integer) As Short
+    Dim proc_num As Integer
+    Dim proc_ary(10000) As String
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim judge As Boolean = False
+        Dim pList() As System.Diagnostics.Process = System.Diagnostics.Process.GetProcesses
+
+        Debug.Print(GetAsyncKeyState(hotkey))
+        judge = GetAsyncKeyState(hotkey)
+
+        If judge = True Then
+            For Each proc As System.Diagnostics.Process In pList
+                For i = 0 To proc_num - 1
+                    If proc.ProcessName = proc_ary(i) Then
+                        proc.Kill()
+                    End If
+                Next
+            Next
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim proc_name As String
+        Dim judge As Boolean = False
+
+        proc_name = InputBox("請輸入程序名稱(不用加上.exe)", "程序輸入")
+
+        If proc_name = "" Then
+            Exit Sub
+        End If
+
+        For i = 0 To proc_num - 1
+            If proc_name = proc_ary(i) Then
+                MsgBox("請勿輸入重複程序", MsgBoxStyle.Exclamation, "錯誤")
+                judge = True
+                Exit Sub
+            End If
+        Next
+
+        If judge = False Then
+            proc_ary(proc_num) = proc_name
+            proc_num += 1
+            If proc_num = 1 Then
+                TextBox1.AppendText(proc_name)
+            Else
+                TextBox1.AppendText(vbNewLine & proc_name)
+            End If
+            Timer1.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        proc_ary(0) = "GTA5"
+        proc_num = 1
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        If proc_num > 1 Then
+            TextBox1.Text = Strings.Left(TextBox1.Text, Strings.Len(TextBox1.Text) - Strings.Len(vbNewLine) - Strings.Len(proc_ary(proc_num - 1)))
+            proc_num -= 1
+        ElseIf proc_num = 1 Then
+            TextBox1.Text = ""
+            proc_num = 0
+            Timer1.Enabled = False
+        End If
+        Debug.Print(proc_num)
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        proc_num = 0
+        TextBox1.Text = ""
+        Timer1.Enabled = False
+    End Sub
+
+    Private Sub TextBox2_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBox2.KeyDown
+        e.SuppressKeyPress = True
+        If msdown = True Then
+            Debug.Print(e.KeyCode.ToString())
+            TextBox2.Text = "按一下變更(目前：" & e.KeyCode.ToString & ")"
+            TextBox2.ForeColor = Color.Black
+            msdown = False
+            hotkey = e.KeyCode
+
+
+        End If
+    End Sub
+
+    Private Sub TextBox2_KeyUp(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles TextBox2.KeyUp
+        Timer1.Enabled = True
+    End Sub
+
+
+    Private Sub TextBox2_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles TextBox2.MouseClick
+        TextBox2.ForeColor = Color.Red
+        TextBox2.Text = "請按下關閉程式熱鍵"
+        Timer1.Enabled = False
+        msdown = True
+    End Sub
+End Class
+
+
